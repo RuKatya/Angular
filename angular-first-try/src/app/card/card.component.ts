42c32311e52f5ead8369edfe7be0c97f854056d9
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2021-12-13 13:22:10 +0200

    card

--- angular-first-try/src/app/card/card.component.ts
+++ angular-first-try/src/app/card/card.component.ts
@@ -3,7 +3,27 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-card',
     templateUrl: './card.component.html',
-    styleUrls: ['./card.component.scss']
+    styleUrls: ['./card.component.scss'],
+    // interpolation: ['@', '@'] //we can change {{}} to any symbole you want
 })
 
-export class CardComponent { }
+export class CardComponent {
+    h2 = `My Card`
+    text = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad ab tempora voluptate itaque voluptatibus nam aperiam
+    quisquam architecto, pariatur officiis dolor fuga quod consectetur consequatur quaerat saepe, modi corporis
+    distinctio.`
+    num = 42
+    array = [1, 2, 3, 4, 5]
+    obj = {
+        name: 'Katya',
+        info:
+        {
+            age: 25,
+            city: 'Ramat Gan'
+        }
+    }
+
+    getInfo() {
+        return `This is my info`
+    }
+}
