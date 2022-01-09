2acad4befbc51caa1e3393d838a689ee0c562e09
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2022-01-09 19:21:43 +0200

    Update position.js

--- MERN/controllers/position.js
+++ MERN/controllers/position.js
@@ -1,15 +1,38 @@
-module.exports.getByCategoryId = function(req, res) {
+const Position = require('../models/Position');
+const errorHandler = require('../utils/errorHandler')
 
+module.exports.getByCategoryId = async function (req, res) {
+    try {
+        const position = await Position.find({
+            category: req.params.categoryId,
+            user: req.user.id
+        })
+        res.status(200).json(position)
+    } catch (error) {
+        errorHandler(res, error)
+    }
 }
 
-module.exports.create = function(req, res) {
+module.exports.create = function (req, res) {
+    try {
 
+    } catch (error) {
+        errorHandler(res, error)
+    }
 }
 
-module.exports.remove = function(req, res) {
+module.exports.remove = function (req, res) {
+    try {
 
+    } catch (error) {
+        errorHandler(res, error)
+    }
 }
 
-module.exports.update = function(req, res) {
+module.exports.update = function (req, res) {
+    try {
 
+    } catch (error) {
+        errorHandler(res, error)
+    }
 }
